d669285094e921026c346162ddaf03350e39dee0
allanahbrisebois <brisebal@sheridancollege.ca>
2020-01-17 11:49:28 -0500

    Second commit

---
+++ src/Class2/Testing.java
@@ -0,0 +1,16 @@
+/*
+ * Name:Allanah Brisebois
+ */
+
+package Class2;
+
+
+public class Testing {
+    public static void main(String[] args){
+        int studentNum;
+        String StudentName;
+    }
+
+    public Testing() {
+    }
+}
